df9d12cda94e8e3840f1d648791345e9ccfb27ac
Jonathon Marolf <jmarolf@microsoft.com>
2018-04-04 02:36:28 -0700

    using ASP.NET Web Site (Razor v3) as the basis for the web template

---
+++ src/Microsoft.VisualStudio.GettingStartedTemplates.Web/Content/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="jquery-__jQueryPackageVersion__.js" />
+/// <reference path="jquery-ui-__jQueryUIPackageVersion__.js" />
+/// <reference path="jquery.validate.js" />
+/// <reference path="jquery.validate.unobtrusive.js" />
+/// <reference path="knockout-__KnockoutPackageVersion__.debug.js" />
+/// <reference path="modernizr-__ModernizrPackageVersion__.js" />
