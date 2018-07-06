bbe510f6850c5c53d8f73361998174eeb98f33e2
Jonathon Marolf <jmarolf@microsoft.com>
2018-07-06 16:01:25 -0700

    update comments

--- src/Templates/GetStarted/Microsoft.VisualStudio.Templates.GetStarted.Console/Content/content.program.cs
+++ src/Templates/GetStarted/Microsoft.VisualStudio.Templates.GetStarted.Console/Content/content.program.cs
@@ -10,8 +10,12 @@ namespace $safeprojectname$
     {
         static void Main(string[] args)
         {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
             Console.ReadKey();
+            
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
--- src/Templates/GetStarted/Microsoft.VisualStudio.Templates.GetStarted.Desktop/Content/content.form1.cs
+++ src/Templates/GetStarted/Microsoft.VisualStudio.Templates.GetStarted.Desktop/Content/content.form1.cs
@@ -8,6 +8,9 @@ $endif$using System.Text;
 $if$ ($targetframeworkversion$ >= 4.5)using System.Threading.Tasks;
 $endif$using System.Windows.Forms;
 
+// This is the code for your desktop app.
+// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+
 namespace $safeprojectname$
 {
     public partial class Form1: Form
@@ -15,6 +18,18 @@ namespace $safeprojectname$
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Click on the link below to continue learning how to build a desktop app using WinForms!
+            System.Diagnostics.Process.Start("http://aka.ms/dotnet-get-started-desktop");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thanks!");
         }
     }
 }
