f111e1634e8c77637e083590582469a10dc7907e
Shen Chen <shech@microsoft.com>
2018-08-24 17:07:17 -0700

    Add the second templates for model library

--- src/Templates/MLDotNet/Microsoft.VisualStudio.Templates.MLDotNet.ModelLibrary/Content/Program.cs
+++ src/Templates/MLDotNet/Microsoft.VisualStudio.Templates.MLDotNet.ModelLibrary/Content/Program.cs
@@ -1,42 +1,43 @@
-/* This template shows how to use a model trained with ML.NET (https://aka.ms/mlnet) to make predictions.
+﻿/* This template shows how to use a model trained with ML.NET (https://aka.ms/mlnet) to make predictions.
  * This model predicts whether a sentence has a positive or negative sentiment. It is based on a sample that can be 
  * found at https://aka.ms/mlnetsentimentanalysis, which provides a more detailed introduction to ML.NET and the scenario.
  * Note that this model was trained on a very small sample dataset, which leads to a relatively low accuracy.
  */
 using System;
+using System.IO;
 using Microsoft.ML;
 using Microsoft.ML.Runtime.Api;
 
 namespace $safeprojectname$
 {
-    class Program
+    public static class ModelPrediction
     {
-        static void Main(string[] args)
+        public static string Predict(SentimentData input)
         {
-            // 1. Load the model from file.
-            var model = PredictionModel.ReadAsync<SentimentData, SentimentPrediction>(@"..\..\..\Models\sentiment_model.zip").Result;
+              // 1. Load the model from file.
+              var model = PredictionModel.ReadAsync<SentimentData, SentimentPrediction>(@"Models\sentiment_model.zip").Result;
 
-            // 2. Use the model for a single prediction.
-            SentimentData testInput = new SentimentData { SentimentText = "ML.NET is fun, more samples at https://github.com/dotnet/machinelearning-samples" };
-            var sentiment = (model.Predict(testInput).Sentiment == true) ? "Positive" : "Negative";
-            Console.WriteLine("Predicted sentiment for \"" + testInput.SentimentText + "\" is:" + sentiment);
-            Console.ReadKey();
-        }
-
-        // Input class that tells ML.NET how to read the input for predictions.
-        public class SentimentData
-        {
-            [Column(ordinal: "0", name: "Label")]
-            public float Sentiment;
-            [Column(ordinal: "1")]
-            public string SentimentText;
-        }
-
-        // Output class for the prediction, in this case including only the predicted sentiment.
-        public class SentimentPrediction
-        {
-            [ColumnName("PredictedLabel")]
-            public bool Sentiment;
-        }
+              // 2. Use the model for a single prediction.
+              var sentiment = (model.Predict(input).Sentiment == true) ? "Positive" : "Negative";
+              Console.WriteLine("Predicted sentiment for \"" + input.SentimentText + "\" is:" + sentiment);
+              return sentiment;
+         }
     }
+
+    // Input class that tells ML.NET how to read the input for predictions.
+    public class SentimentData
+    {
+        [Column(ordinal: "0", name: "Label")]
+        public float Sentiment;
+        [Column(ordinal: "1")]
+        public string SentimentText;
+    }
+
+    // Output class for the prediction, in this case including only the predicted sentiment.
+    public class SentimentPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Sentiment;
+    }
+
 }
